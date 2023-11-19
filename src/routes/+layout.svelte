ddbb204043df19f9076ba5b5fd7f53cd8208a304
mou <detective0218gm@gmail.com>
2023-11-19 22:33:46 +0900

    add header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Layout from './_layout/index.svelte';
+</script>
+
+<Layout>
+	<slot />
+</Layout>
---
+++ src/routes/_layout/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import './global.css';
+</script>
+
+<header>
+	<a href="/">お酒メモ</a>
+	<nav>
+		<a href="/country">国別</a>
+	</nav>
+</header>
+
+<slot />
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		font-size: var(--font-size-2);
+	}
+
+	nav {
+		font-size: var(--font-size-1);
+	}
+</style>
