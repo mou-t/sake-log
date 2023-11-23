519e5977a920249bac9322a0e12c550cd6d9ab3b
mou <detective0218gm@gmail.com>
2023-11-23 23:43:52 +0900

    レイアウトシフトを軽減

--- src/lib/ui/card.svelte
+++ src/lib/ui/card.svelte
@@ -18,10 +18,14 @@
 <style>
 	.img-wrapper {
 		width: 100%;
+		aspect-ratio: 3/4;
 		overflow: hidden;
 	}
 
 	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
 		border-radius: 10px;
 		transition: all 0.7s;
 	}
