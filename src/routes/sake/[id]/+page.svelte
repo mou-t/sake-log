389b595ea7fb5e1b4fc8ca7c2fc03739989583a6
mou <detective0218gm@gmail.com>
2023-11-24 23:52:11 +0900

    個別ページのスタイルを追加

--- src/routes/sake/[id]/+page.svelte
+++ src/routes/sake/[id]/+page.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
+	import { generateSrc, generateSrcset } from '$lib/utils/microcms-image.js';
+
 	export let data;
 </script>
 
-<h2>
-	id: {data.id}
-</h2>
+<article>
+	<div class="img-wrapper">
+		<img src={generateSrc(data.image.url, { w: '2000' })} alt={data.name} />
+	</div>
+	<div class="info">
+		<h2>
+			{data.name}
+		</h2>
+		<p>国： {data.country.japanese}</p>
+	</div>
+</article>
 
-<p>{data.name}</p>
+<style>
+	article {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.img-wrapper,
+	.info {
+		margin: 0 auto;
+		width: var(--size-10);
+	}
+
+	img {
+		aspect-ratio: 3/4;
+		width: 100%;
+	}
+</style>
