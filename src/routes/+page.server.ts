48307de13942974df4e52e3c35df1582ad29485b
mou <detective0218gm@gmail.com>
2023-11-29 22:54:36 +0900

    microcmsのlimitを100に変更

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,6 @@ import { getSakeList } from '$lib/server/microcms-client';
 export const prerender = true;
 
 export const load = async () => {
-	const sakeList = await getSakeList();
+	const sakeList = await getSakeList({ limit: 100 });
 	return { sakeList };
 };
