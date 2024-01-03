d4ff9a6d6c463a0153b55c7d30f93d17113fb519
mou <detective0218gm@gmail.com>
2024-01-03 19:04:16 +0900

    add comments

--- src/lib/server/microcms-client/get-placeholder-image.ts
+++ src/lib/server/microcms-client/get-placeholder-image.ts
@@ -2,6 +2,10 @@ import { generateSrc } from '$lib/utils/microcms-image';
 
 export const getPlaceHolderBase64 = async (imageUrl: string) => {
 	const buffer = await fetch(generateSrc(imageUrl, { w: '8', fm: 'png' })).then(async (res) =>
+		// node依存のコード
+		// ビルド時にしか使用しなければ、実行時エラーは発生しない。
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error
 		Buffer.from(await res.arrayBuffer())
 	);
 
